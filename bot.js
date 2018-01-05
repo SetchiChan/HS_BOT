7c707a832c389a5d846344c915ae61349dc25d28
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-01-05 16:23:20 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const newUser = [];
+
+//sending dm
+bot.on('guildMemberAdd', member => {
+    member.send("Welcome to the server!");
+ });
+
+//Welcome to new member chat
+bot.on('guildMemberAdd', member => {
+    bot.channels.get("341921678118617101").sendMessage(member + " has joined Heaven Shadow! Please connect to new members lobby and wait for staff to orientate you.");
+});
+
+//Staff notification of new user
+bot.on('guildMemberAdd', member => {
+    bot.channels.get('342366838451339264').sendMessage(member + " has joined the Heaven Shadow. Please contact them for orientation!");
+});
+
+//Staff notification user has left
+bot.on('guildMemberRemove', member => {
+    bot.channels.get('342366838451339264').sendMessage(member + " has left Heaven Shadow.");
+});
+
+//Nickname
+bot.on('guildMemberAdd', member => {
+    member.setNickname("🆕" + member.displayName)
+});
+
+//commands
+bot.on('message', (message) => {
+
+    if(message.content == '!help') {
+        message.reply('Sorry, We do not have commands right now.');
+        //message.channel.sendMessage('Sorry, We do not have commands right now.);
+    }
+
+});
+
+bot.login(process.env.Mzk4NzEzMTQ2Mjk1OTc1OTM2.DTC4hg.a3b37hPy0JcGr-hcdqtcgZ1RyM0);
