f43e935bb37e56f15bbb370f9e9800dfa94e81a8
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-01-06 03:03:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,31 +125,6 @@ bot.on("message", function(message) {
         case "ping":
             message.channel.sendMessage("Pong!");
             break;
-        case "help":
-            message.channel.send({embed: {
-                color: 3447003,
-                author: {
-                    name: client.user.username,
-                    icon_url: client.user.avatarURL
-                },
-                title: "This is an embed",
-                url: "http://google.com",
-                description: "This is a test embed to showcase what they look like and what they can do.",
-                fields: [{
-                    name: "Fields",
-                    value: "They can have different fields with small headlines."
-                  },
-                  {
-                    name: "Masked links",
-                    value: "You can put [masked links](http://google.com) inside of rich embeds."
-                  }
-                ],
-                timestamp: new Date(),
-                footer: {
-                    icon_url: client.user.avatarURL,
-                    text: "@HS Bot"
-                }
-               }
 });
             
 bot.login(process.env.BOT_TOKEN);
