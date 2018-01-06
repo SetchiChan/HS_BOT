435f58b30b82bb1d98aafe61a71e24631f584c73
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-01-05 19:20:25 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const newUser = [];
+
+//sending dm
+bot.on('guildMemberAdd', member => {
+    member.send("Welcome to the server!");
+ });
+
+//Welcome to new member chat
+bot.on('guildMemberAdd', member => {
+    bot.channels.get("341921678118617101").sendMessage(member + " has joined Heaven Shadow! Please connect to new members lobby and wait for staff to orientate you.");
+});
+
+//Staff notification of new user
+bot.on('guildMemberAdd', member => {
+    bot.channels.get('342366838451339264').sendMessage(member + " has joined the Heaven Shadow. Please contact them for orientation!");
+});
+
+//Staff notification user has left
+bot.on('guildMemberRemove', member => {
+    bot.channels.get('342366838451339264').sendMessage(member + " has left Heaven Shadow.");
+});
+
+//Nickname
+bot.on('guildMemberAdd', member => {
+    member.setNickname("🆕" + member.displayName)
+});
+
+bot.login(process.env.BOT_TOKEN);
