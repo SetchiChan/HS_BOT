faa364e3d0fbbfc97ee689c5b59167ebd746cce5
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-01-06 03:07:56 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,6 @@ const bot = new Discord.Client();
 
 const newUser = [];
 
-const PREFIX = "'";
-const TOKEN = "x";
-
 //sending dm
 bot.on('guildMemberAdd', member => {
     member.send("Welcome to the server!");
@@ -30,101 +27,14 @@ bot.on('guildMemberRemove', member => {
 bot.on('guildMemberAdd', member => {
     member.setNickname("🆕" + member.displayName)
 });
-              
+
 //Random Message
 bot.on('message', (message) => {
     
     if(message.content == 'bad bot') {
         message.reply('Ive been a very naughty bot');
     }
-});
-
-//commands
-bot.on("ready", function() {
-    console.log("Ready");
-});
-
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
-
-    if (!message.content.startsWith(PREFIX)) return;
-
-    var args = message.content.substring(PREFIX.length).split(" ");
-
-    switch (args[0].toLowerCase()) {
-        case "info":
-            message.channel.sendMessage("I'm a fucking useless Bot, totally not created by Setchi");
-            break;
-        default:
-            message.channel.sendMessage("Invalid command");
-            break;
-        case "ping":
-            message.channel.sendMessage("Pong!");
-            break;
-        case "help":
-   const Discord = require('discord.js');
-const bot = new Discord.Client();
-
-const newUser = [];
-
-const PREFIX = "'";
-const TOKEN = "x";
-
-//sending dm
-bot.on('guildMemberAdd', member => {
-    member.send("Welcome to the server!");
- });
-
-//Welcome to new member chat
-bot.on('guildMemberAdd', member => {
-    bot.channels.get("399041704604467212").sendMessage(member + " has joined Heaven Shadow! Please connect to new members lobby and wait for staff to orientate you.");
-});
-
-//Staff notification of new user
-bot.on('guildMemberAdd', member => {
-    bot.channels.get('399039851288068096').sendMessage(member + " has joined the Heaven Shadow. Please contact them for orientation!");
-});
-
-//Staff notification user has left
-bot.on('guildMemberRemove', member => {
-    bot.channels.get('399039851288068096').sendMessage(member + " has left Heaven Shadow.");
-});
-
-//Nickname
-bot.on('guildMemberAdd', member => {
-    member.setNickname("🆕" + member.displayName)
-});
-              
-//Random Message
-bot.on('message', (message) => {
     
-    if(message.content == 'bad bot') {
-        message.reply('Ive been a very naughty bot');
-    }
 });
 
-//commands
-bot.on("ready", function() {
-    console.log("Ready");
-});
-
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
-
-    if (!message.content.startsWith(PREFIX)) return;
-
-    var args = message.content.substring(PREFIX.length).split(" ");
-
-    switch (args[0].toLowerCase()) {
-        case "info":
-            message.channel.sendMessage("I'm a fucking useless Bot, totally not created by Setchi");
-            break;
-        default:
-            message.channel.sendMessage("Invalid command");
-            break;
-        case "ping":
-            message.channel.sendMessage("Pong!");
-            break;
-});
-            
 bot.login(process.env.BOT_TOKEN);
