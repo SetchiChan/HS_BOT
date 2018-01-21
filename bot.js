faca50d5ab6a1e10091d61431421c611810a06b3
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-01-20 20:15:27 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -82,16 +82,23 @@ bot.on("message", function(message) {
         case "ping":
             message.channel.sendMessage("Pong!");
             break;
-        case "pref":
-            message.channel.sendMessage("Command does not work. 'pref is in progress.")
-            .then(() => message.react('399268521776906241'))
-            .then(() => message.react('399268521646882816'))
-            .then(() => message.react('399268521638494208'))
-            .then(() => message.react('399268521420521474'))
-            .then(() => message.react('399268521613328384'))
-            .then(() => message.react('399268521260875778'))
-            .catch(() => console.error('One of the emojis failed to react.'));
-            break;
+        case "verify":
+            message.channel.send({embed: {
+                color: 10038562,
+                author: {
+                    name: bot.user.username,
+                    icon_url: bot.user.avatarURL
+                },
+                title: "Verification system",
+                description: "================",
+                fields: [{
+                    name: "How to start",
+                    description: "To begin, please type in your region. [NA] or [EUW] (Do not include [])"
+                }
+            ],
+            }
+        })
+        break;
         case "help":
             message.channel.send({embed: {
                 color: 10038562,
