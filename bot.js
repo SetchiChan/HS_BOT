cb88c6a82c8ca95323e847ce7408f07a51585bec
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-01-21 23:44:19 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -104,7 +104,7 @@ bot.on("message", function(message) {
             .then(function (message) {
                 message.react('404452656220864512')
 
-                if (message.member.reaction.find("name", '404452656220864512')) {
+                if (member.reaction.find("name", "404452656220864512")) {
                     message.channel.sendMessage("LOL")
                 }
             })
