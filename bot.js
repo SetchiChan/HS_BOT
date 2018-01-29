6ff6317475749281971c74051dc456d7787b020b
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-01-28 21:52:52 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -103,10 +103,10 @@ bot.on("message", function(message) {
             })
             const collector = message.createReactionCollector((reaction, user) =>
             user.id === message.author.id &&
-            reaction.message.name === ('404452656220864512')
+            reaction.emoji.id === "404452656220864512"
             ).once("collect", reaction => {
                 const chosen = message.react;
-                if(chosen === '404452656220864512'){
+                if(chosen === "404452656220864512"){
                     message.channel.send("Fuck")
                 }
                 collector.stop();
